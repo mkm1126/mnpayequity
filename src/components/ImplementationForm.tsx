4abdf92814e5f8be84ab6f96dfb589135dc909ec
mkm1126 <jamie.kriegel@gmail.com>
2025-11-11 14:22:00 -0600

    Updated ImplementationForm.tsx

--- src/components/ImplementationForm.tsx
+++ src/components/ImplementationForm.tsx
@@ -314,7 +314,7 @@ export function ImplementationForm({
               disabled={isSubmitted}
               maxLength={60}
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#003865] focus:border-transparent disabled:bg-gray-100"
-              placeholder="e.g., John Smith"
+              placeholder="name of chief elected official"
             />
           </div>
 
